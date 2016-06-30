11cdcc30936847339da68975fe22fbf7aaa7714d
Boris <boris.chlopkov@hotmail.be>
2016-06-30 21:40:31 +0200

    basic frontend app setup

---
+++ unter-front-app/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('unterFrontApp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ unter-front-app/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('unterFrontApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
