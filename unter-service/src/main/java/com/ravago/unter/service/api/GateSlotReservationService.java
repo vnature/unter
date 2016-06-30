3f0cdaa148df51980d362348db63c3c00ee8df67
kvervecken <kris.vervecken@ravago.com>
2016-06-30 22:01:16 +0200

    Test push

--- unter-service/src/main/java/com/ravago/unter/service/api/GateSlotReservationService.java
+++ unter-service/src/main/java/com/ravago/unter/service/api/GateSlotReservationService.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface GateSlotReservationService {
 
 	String reserveGateSlot(GateSlotReservationCommand gsr);
-	
+	//
 	GateSlotReservationResult getGateSlotReservation(String reservationNo);
 	
 	List<GateSlotReservationResult> listGateSlotReservations(String warehouse, Date loadDate);
